eafc207c66b5d9db21bb45f621213dcc29c16721
Atharva-1512 <atharvagade403@gmail.com>
2026-02-27 11:37:23 +0530

    Voter registration system completed

---
+++ .history/voter/register_20260227112411.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Voter Registration</title>
+</head>
+<body>
+
+<h2>Register as Voter</h2>
+
+<form method="POST" action="register_process.php">
+
+<input type="text" name="name" placeholder="Full Name" required><br><br>
+
+<input type="email" name="email" placeholder="Email" required><br><br>
+
+<input type="password" name="password" placeholder="Password" required><br><br>
+
+<button type="submit">Register</button>
+
+</form>
+
+</body>
+</html>
---
+++ .history/voter/register_process_20260227113018.php
@@ -0,0 +1,21 @@
+<?php
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+include '../config/db.php';
+include '../config/db.php';
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+
+$sql = "INSERT INTO voters (name, email, password)
+        VALUES ('$name', '$email', '$password')";
+
+if($conn->query($sql)){
+    echo "Registration Successful!";
+}else{
+    echo "Error: " . $conn->error;
+}
+?>
